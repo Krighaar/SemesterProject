0133d4e26164891f5efdbb41cc613d79788e465d
tomoetom <tomoemp@gmail.com>
2014-11-26 10:46:09 +0100

    put is added

--- server/routes/REST_Admin_API.js
+++ server/routes/REST_Admin_API.js
@@ -47,5 +47,29 @@ router.post("/", function (req, res, next) {
     })
 
 })
+router.put("/:_id", function (req, res, next) {
 
+    var id=req.params._id;
+
+    if(typeof global.mongo_error !== "undefined"){
+        res.status(500);
+        res.end("Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
+        return;
+    }
+
+
+
+    user.findByIdAndUpdate(id,req.body,function (err, changedWish) {
+        if (err) {
+            res.status(err.status || 400);
+            res.end(JSON.stringify({error: err.toString()}));
+            return;
+        }
+        res.header("Content-type","application/json");
+
+        res.end(JSON.stringify(changedWish));
+
+    })
+
+})
 module.exports = router;
