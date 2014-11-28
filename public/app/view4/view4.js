298f78c28f8d27c8c91325f5e04fce942f109b18
Peter K <millipeter15@hotmail.com>
2014-11-28 09:44:49 +0100

    Found error in view4 controller that crashed controller in view3

--- public/app/view4/view4.js
+++ public/app/view4/view4.js
@@ -8,6 +8,22 @@ angular.module('myAppRename.view4', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view4', {
             templateUrl: 'app/view4/view4.html',
-            controller: 'View3Ctrl'
+            controller: 'View4Ctrl'
         });
-    }])
+    }]).controller('View4Ctrl', ['$scope', 'wishFactory', function ($scope, wishFactory) {
+
+        wishFactory.getWish()
+            .success(function (friends) {
+                $scope.fList = friends;
+            })
+            .error(function (error) {
+                $scope.status = 'Unable to load customer data: ' + error.message;
+            });
+
+        $scope.update = function() {
+
+                $scope.whi = $scope.item
+            console.log($scope.whi)
+
+        }
+    }]);
